c4025d327a403e6171c3e3ff8f03eaf1c07c2d2e
Malith Madushan Liyanage <99232266+IT21280474@users.noreply.github.com>
2023-05-16 12:20:18 +0530

    version 2.0

---
+++ Desigs Adding Updating Deleting/app/src/main/java/com/example/app/models/DesignModel.kt
@@ -0,0 +1,10 @@
+package com.example.app.models
+
+data class DesignModel (
+    var designId: String? = null,
+    var designName:String? = null,
+    var designDescription:  String? = null,
+    var designPrice: String? = null
+) {
+
+}
