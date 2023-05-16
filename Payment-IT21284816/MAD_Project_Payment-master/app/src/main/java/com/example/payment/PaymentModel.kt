d594e9118ef834952a6a33e86dd2894bc7bbc0b2
IT21284816 <99232799+IT21284816@users.noreply.github.com>
2023-05-16 09:05:47 +0530

    delete

--- Payment-IT21284816/MAD_Project_Payment-master/app/src/main/java/com/example/payment/PaymentModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.payment
-
-data class PaymentModel(
-    var cardId: String? = null,
-    var cardNum: String? = null,
-    var expireyDate: String? = null,
-    var cardCVV: String? = null,
-    var cardName: String? = null
-)
